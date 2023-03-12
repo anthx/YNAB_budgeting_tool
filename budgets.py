16cdcb4a3902c12afcb2f783642956fe903c11eb
Anthony Carrick <anthx@users.noreply.github.com>
2023-03-12 12:26:06 +1000

    basic adding functionality

--- budgets.py
+++ budgets.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from dataclasses import dataclass
+from textwrap import indent
 from ynab import YNAB
 import datetime
 from typing import List
@@ -11,9 +12,10 @@ class Category(object):
     categories: List
     isMaster: bool
     amount: float
+    budgeted: float
 
     def __repr__(self) -> str:
-        out = f"Master:{self.isMaster} \n Name: {self.name} \n Amount: ${self.amount} \n"
+        out = f"Master:{self.isMaster} \n Name: {self.name} \n Amount: ${self.amount} Budget: ${self.budgeted}\n"
         out += "    " f"{self.categories}"
         return out
 
@@ -22,20 +24,26 @@ class Category(object):
             if each.name == name:
                 return each
 
-budget = Category(name="budget", categories=[], isMaster=False, amount=None)
+budget = Category(name="budget", categories=[], isMaster=False, amount=None, budgeted=None)
 for m in ynab.master_categories:
-    this_master = Category(m.name, [], True, 0)
-    budget.categories.append(this_master)
-    for c in m.categories:
-        if c.name != "Hidden Categories":
-            this_category = Category(c.name, [], False, 0)
-            this_master.categories.append(this_category)
+    if m.name != "Hidden Categories":
+        this_master = Category(m.name, [], True, 0, 0)
+        budget.categories.append(this_master)
+        for c in m.categories:
+                this_category = Category(c.name, [], False, 0, c.cached_balance)
+                this_master.categories.append(this_category)
+                this_master.budgeted += c.cached_balance
 
 
 for a in ynab.accounts:
     for tran in a.transactions.since(datetime.date.today().replace(day=1).isoformat()):
         if tran.category:
-            budget.Get_Child_Category(tran.category.master_category.name).amount += tran.amount
-            budget.Get_Child_Category(tran.category.master_category.name).Get_Child_Category(tran.category.name).amount += tran.amount
+            tran_cat = budget.Get_Child_Category(tran.category.master_category.name)
+            tran_cat.amount += tran.amount
+            tran_cat.budgeted += abs(tran.amount)
+
+            tran_master_cat = budget.Get_Child_Category(tran.category.master_category.name).Get_Child_Category(tran.category.name)
+            tran_master_cat.amount += tran.amount
+            tran_master_cat.budgeted += abs(tran.amount)
 
 print(budget)
